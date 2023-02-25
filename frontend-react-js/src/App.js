ba14b3a84de9e18393a5b9926687b56714b78dba
Quazi Ahmed <dev.quazi@gmail.com>
2023-02-25 08:47:25 +0000

    update app.js

--- frontend-react-js/src/App.js
+++ frontend-react-js/src/App.js
@@ -25,6 +25,10 @@ const router = createBrowserRouter([
     element: <NotificationsFeedPage />
   },
   {
+    path: "/@: handle",
+    element: <UserFeedPage />
+  },
+  {
     path: "/messages",
     element: <MessageGroupsPage />
   },
