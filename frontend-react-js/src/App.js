270154fa37adcaa32e4700fc3b46ef20384cb563
Quazi Ahmed <dev.quazi@gmail.com>
2023-02-26 04:28:56 +0000

    update frontend n backend

--- frontend-react-js/src/App.js
+++ frontend-react-js/src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 
 import HomeFeedPage from './pages/HomeFeedPage';
-import NotificationsFeedPage from './pages/NotificationsFeedPage';
 import UserFeedPage from './pages/UserFeedPage';
 import SignupPage from './pages/SignupPage';
 import SigninPage from './pages/SigninPage';
@@ -10,6 +9,7 @@ import MessageGroupsPage from './pages/MessageGroupsPage';
 import MessageGroupPage from './pages/MessageGroupPage';
 import ConfirmationPage from './pages/ConfirmationPage';
 import React from 'react';
+import process from 'process';
 import {
   createBrowserRouter,
   RouterProvider
@@ -21,11 +21,7 @@ const router = createBrowserRouter([
     element: <HomeFeedPage />
   },
   {
-    path: "/notifications",
-    element: <NotificationsFeedPage />
-  },
-  {
-    path:"/@: handle",
+    path: "/@:handle",
     element: <UserFeedPage />
   },
   {
